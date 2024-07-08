3b198a7b40591c62de1b41e917600c4791d361c1
cf <46063145+cf0615@users.noreply.github.com>
2024-07-08 23:46:52 +0800

    Add files via upload

--- save_data_mongodb.py
+++ save_data_mongodb.py
@@ -5,7 +5,9 @@ import json
 
 #to upload all dataset into the mongodb
 cases_malaysia = pd.read_csv('C:/Users/junch/OneDrive/Documents/BigData/Project/Preprocess/cases_malaysia_preprocessed.csv')
-case_state = pd.read_csv('C:/Users/junch/OneDrive/Documents/BigData/Project/Preprocess/caseState_preprocessed.csv', parse_dates=['date'])
+case_state = pd.read_csv('C:/Users/junch/OneDrive/Documents/BigData/Project/Preprocess/caseState_preprocessed.csv')
+deaths_malaysia = pd.read_csv('C:/Users/junch/OneDrive/Documents/BigData/Project/Preprocess/deaths_malaysia_preprocessed.csv')
+icu = pd.read_csv('C:/Users/junch/OneDrive/Documents/BigData/Project/Preprocess/icu_preprocessed.csv')
 
 # Path to the GeoJSON file
 geojson_path = 'C:/Users/junch/OneDrive/Documents/BigData/Project/Dashboard/malaysia.geojson'  
@@ -16,4 +18,5 @@ with open(geojson_path) as f:
 mh.save_data_to_db(cases_malaysia, 'cases_malaysia', 'CovidData')
 mh.save_data_to_db(case_state, 'cases_state', 'CovidData')
 mh.save_data_to_db(geojson_data, 'malaysia_geojson', 'CovidData')
-
+mh.save_data_to_db(deaths_malaysia, 'deaths_malaysia', 'CovidData')
+mh.save_data_to_db(icu, 'icu', 'CovidData')
